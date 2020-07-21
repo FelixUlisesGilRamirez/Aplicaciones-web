f480b59a789fd5d12222e5426ef6859522077d3a
FelixUlisesGilRamirez <al221911533@gmail.com>
2020-07-20 19:43:38 -0500

    modificaciones

---
+++ Base de Datos/asignacion.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = "hello";
+$a .= "world";
+var_dump ($a);
+
+$b = 5;
+$b *= 4;
+
+var_dump($b);
+
+
+$b = 5;
+$b -= 4;
+
+var_dump($b);
+
+¿>
---
+++ Base de Datos/foreach.php
@@ -0,0 +1,13 @@
+<?php
+$array = [
+    "uno" => 1,
+    "dos" => 2,
+    "tres" => 3,
+];
+   var_dump(array);
+
+   foreach ($array as $key => value) {
+       echo "</br>";
+       echo "el valor de $key es: $value";
+   }
+¿>
